c2e412b997baf03941535ca1905a1c07245558a5
Nico Aguerre <naguere@fi.uba.ar>
2022-11-09 02:16:10 -0300

    Connects core service with db service on kubernetes

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,15 @@
 import express from 'express'
+import knex from 'knex'
 
 const app = express();
+const knex_connection = knex({
+    client: 'pg',
+    connection: 'postgresql://postgres:test123@postgres-db-internal:5432/postgres'
+});
+
+knex_connection.raw('SELECT 1').then(() => {console.log("Connection established!")}).catch((e) => console.log(`Error: ${e}`));
 
 app.listen(3000, () => {
     console.log("App listening on 3000");
+    console.log(knex_connection.client.connectionSettings);
 });
