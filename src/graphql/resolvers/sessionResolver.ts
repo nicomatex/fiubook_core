1f689c6674e915a7dce5ffec4a29a8a59d67ca5f
Nico Aguerre <naguere@fi.uba.ar>
2022-11-13 17:57:55 -0300

    Adds session creation token

--- src/graphql/resolvers/sessionResolver.ts
+++ src/graphql/resolvers/sessionResolver.ts
@@ -1,16 +1,10 @@
-import {
-    Arg,
-    Field,
-    InputType,
-    Mutation,
-    Resolver,
-    UseMiddleware,
-} from 'type-graphql'
+import { Arg, Mutation, Resolver, UseMiddleware } from 'type-graphql'
 
-import { MaxLength } from 'class-validator'
 import { Session } from '@graphql/schemas/session'
 import CheckFiubaCredentialsMiddleware from '@graphql/middlewares/checkFIUBACredentialsMiddleware'
 import { Credentials } from '@graphql/types'
+import userRepository from '@repositories/userRepository'
+import { createSessionToken } from '@util/authUtil'
 
 @Resolver(Session)
 class SessionResolver {
@@ -21,8 +15,10 @@ class SessionResolver {
     async createSession(
         @Arg('credentials') credentials: Credentials
     ): Promise<Session> {
+        const user = await userRepository.getUserByDNI(credentials.dni)
+        const token = createSessionToken(user)
         return {
-            token: 'falopa-123-456',
+            token,
         }
     }
 }
