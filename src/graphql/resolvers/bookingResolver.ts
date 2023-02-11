53acebd4bbbb42aa5ad43390042261102a76cd18
Nicolás Aguerre <naguerre@fi.uba.ar>
2023-02-11 19:51:01 -0300

    Fix import issue

--- src/graphql/resolvers/bookingResolver.ts
+++ src/graphql/resolvers/bookingResolver.ts
@@ -12,6 +12,7 @@ import {
 import { BookingStatus, LoggedInContextType } from '@graphql/types';
 import {
     Booking,
+    BookingEdgesType,
     CreateBookingArgs,
     CreateBookingResponse,
     PaginatedBookingResponse,
@@ -27,39 +28,37 @@ class BookingResolver {
     @Mutation(() => CreateBookingResponse)
     async createBooking(
         @Arg('creationArgs') creationArgs: CreateBookingArgs,
-        @Ctx() ctx: LoggedInContextType
+        @Ctx() ctx: LoggedInContextType,
     ): Promise<CreateBookingResponse> {
         if (creationArgs.start_date.getTime() < Date.now()) {
             throw new Error('Start date cannot be in the past.');
         }
         if (creationArgs.end_date <= creationArgs.start_date) {
             throw new Error(
-                'End date cannot be equal or earlier than start date'
+                'End date cannot be equal or earlier than start date',
             );
         }
-        const conflictingBookings =
-            await bookingRepository.getConflictingBookings(
-                creationArgs.service_id,
-                creationArgs.start_date,
-                creationArgs.end_date
-            );
+        const conflictingBookings = await bookingRepository.getConflictingBookings(
+            creationArgs.service_id,
+            creationArgs.start_date,
+            creationArgs.end_date,
+        );
 
         if (conflictingBookings.length > 0) {
             throw new Error(
-                'There are conflicts with other bookings in the selected time interval'
+                'There are conflicts with other bookings in the selected time interval',
             );
         }
 
         const requestedService = await serviceRepository.getServiceById(
-            creationArgs.service_id
+            creationArgs.service_id,
         );
 
-        const insertedBookingEdge: BookingEdgesType =
-            await bookingRepository.createBooking(
-                creationArgs,
-                requestedService,
-                ctx.userId
-            );
+        const insertedBookingEdge: BookingEdgesType = await bookingRepository.createBooking(
+            creationArgs,
+            requestedService,
+            ctx.userId,
+        );
 
         return { bookingEdge: insertedBookingEdge };
     }
@@ -69,20 +68,19 @@ class BookingResolver {
     async conflictingBookings(
         @Arg('service_id') serviceId: string,
         @Arg('start_date') startDate: Date,
-        @Arg('end_date') endDate: Date
+        @Arg('end_date') endDate: Date,
     ): Promise<Booking[]> {
         if (endDate <= startDate) {
             throw new Error(
-                'End date cannot be equal or earlier than start date'
+                'End date cannot be equal or earlier than start date',
             );
         }
 
-        const conflictingBookings =
-            await bookingRepository.getConflictingBookings(
-                serviceId,
-                startDate,
-                endDate
-            );
+        const conflictingBookings = await bookingRepository.getConflictingBookings(
+            serviceId,
+            startDate,
+            endDate,
+        );
 
         return conflictingBookings;
     }
@@ -92,12 +90,12 @@ class BookingResolver {
     async myBookings(
         @Ctx() ctx: LoggedInContextType,
         @Arg('after', { nullable: true }) paginationToken?: string,
-        @Arg('first', { nullable: true }) pageSize?: number
+        @Arg('first', { nullable: true }) pageSize?: number,
     ) {
         const res = await bookingRepository.getBookingsByRequestorId(
             ctx.userId,
             paginationToken,
-            pageSize
+            pageSize,
         );
         return res;
     }
@@ -106,21 +104,21 @@ class BookingResolver {
     @Authorized()
     async cancelBooking(
         @Arg('booking_id') bookingId: string,
-        @Ctx() ctx: LoggedInContextType
+        @Ctx() ctx: LoggedInContextType,
     ) {
         const booking = await bookingRepository.getBookingById(bookingId);
 
         // Both the publisher and the requestor may cancel
         if (
-            booking.requestor_id !== ctx.userId &&
-            booking.publisher_id !== ctx.userId
+            booking.requestor_id !== ctx.userId
+            && booking.publisher_id !== ctx.userId
         ) {
             throw new Error('You are not authorized for this action.');
         }
 
         const res = await bookingRepository.updateBookingStatusById(
             bookingId,
-            BookingStatus.CANCELLED
+            BookingStatus.CANCELLED,
         );
         return res;
     }
@@ -130,12 +128,12 @@ class BookingResolver {
     async myBookingsForPublisher(
         @Ctx() ctx: LoggedInContextType,
         @Arg('after', { nullable: true }) paginationToken?: string,
-        @Arg('first', { nullable: true }) pageSize?: number
+        @Arg('first', { nullable: true }) pageSize?: number,
     ) {
         const res = await bookingRepository.getBookingsByPublisherId(
             ctx.userId,
             paginationToken,
-            pageSize
+            pageSize,
         );
         return res;
     }
@@ -145,7 +143,7 @@ class BookingResolver {
     async acceptBooking(
         @Arg('booking_id') bookingId: string,
         @Arg('accept') accept: boolean,
-        @Ctx() ctx: LoggedInContextType
+        @Ctx() ctx: LoggedInContextType,
     ) {
         const booking = await bookingRepository.getBookingById(bookingId);
         if (booking.publisher_id !== ctx.userId) {
@@ -154,13 +152,13 @@ class BookingResolver {
 
         if (booking.booking_status !== BookingStatus.PENDING_CONFIRMATION) {
             throw new Error(
-                `Booking with id ${bookingId} is not in pending confirmation state.`
+                `Booking with id ${bookingId} is not in pending confirmation state.`,
             );
         }
 
         const res = await bookingRepository.updateBookingStatusById(
             bookingId,
-            accept ? BookingStatus.CONFIRMED : BookingStatus.CANCELLED
+            accept ? BookingStatus.CONFIRMED : BookingStatus.CANCELLED,
         );
 
         return res;
@@ -169,24 +167,20 @@ class BookingResolver {
     @FieldResolver()
     async service(@Root() booking: Booking) {
         const service = await serviceRepository.getServiceById(
-            booking.service_id
+            booking.service_id,
         );
         return service;
     }
 
     @FieldResolver(() => User)
     async requestor(@Root() booking: Booking) {
-        const requestor = await userRepository.getUserById(
-            booking.requestor_id
-        );
+        const requestor = await userRepository.getUserById(booking.requestor_id);
         return requestor;
     }
 
     @FieldResolver(() => User)
     async publisher(@Root() booking: Booking) {
-        const publisher = await userRepository.getUserById(
-            booking.publisher_id
-        );
+        const publisher = await userRepository.getUserById(booking.publisher_id);
         return publisher;
     }
 }
